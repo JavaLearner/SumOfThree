54f21ce7eada2d5e264a01351305ef7c6d5c6890
JavaLearner <slavav2007@gmail.com>
2015-08-26 15:06:15 +0300

    add

---
+++ SumOfThree/SumOfThree/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SumOfThree
+{
+    public partial class SumForm : Form
+    {
+        public SumForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            string firstOperand = textBox1.Text;
+            string secondOperand = textBox2.Text;
+            string thirdOperand = textBox3.Text;
+
+
+            try
+            {
+                var newSum = new SumAlgorithm();
+                newSum.calculateSum(Int32.Parse(firstOperand),
+                    Int32.Parse(secondOperand), Int32.Parse(thirdOperand));
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input.");
+            }
+        }
+
+
+
+        public void viewResult(int num1, int num2, int num3)
+        {
+            StringBuilder resultMessage = new StringBuilder();
+            resultMessage.Append(num1.ToString());
+            resultMessage.Append(num2.ToString());
+            resultMessage.Append(num3.ToString());
+            MessageBox.Show(resultMessage.ToString());
+
+        }
+
+    }
+}
---
+++ SumOfThree/SumOfThree/SumAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfThree
+{
+    class SumAlgorithm
+    {
+        public SumAlgorithm(){
+            }
+
+        public int firstOp { get; set; }
+        public int secondOp { get; set; }
+        public int thirdOp { get; set; }
+
+        public void calculateSum(int firstOp, int secondOp, int thirdOp)
+        {
+            var newForm = new SumForm();
+            newForm.viewResult(firstOp * secondOp, firstOp * thirdOp, 
+                reverseINum((firstOp + thirdOp) * secondOp));
+
+        }
+
+        private int reverseINum(int num)
+        {
+            String str = num.ToString();
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            str = new String (arr);
+            num = Int32.Parse(str);
+            return num;
+        }
+    }
+}
